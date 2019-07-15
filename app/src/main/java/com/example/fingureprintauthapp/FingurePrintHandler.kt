8b9bcac3d7ead60e1b6c38f9cf3e4a211e1164da
moseskamira <moses.african@gmail.com>
2019-07-15 16:43:58 +0300

    Implement Fingure print - Implement Fingure print in Android App

---
+++ app/src/main/java/com/example/fingureprintauthapp/FingurePrintHandler.kt
@@ -0,0 +1,45 @@
+package com.example.fingureprintauthapp
+
+import android.app.Activity
+import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat
+import android.widget.TextView
+
+class FingerPrintHandler(val context: Context) : FingerprintManagerCompat.AuthenticationCallback() {
+
+    private val cancellationSignal = android.support.v4.os.CancellationSignal()
+
+    fun startAuth(fingerprintManager: FingerprintManagerCompat, cryptoObject: FingerprintManagerCompat.CryptoObject?) {
+        fingerprintManager.authenticate(cryptoObject,0, cancellationSignal , this, null)
+    }
+
+    override fun onAuthenticationError(errMsgId: Int, errString: CharSequence?) {
+        super.onAuthenticationError(errMsgId, errString)
+        this.update("There was an Authentication Error$errString", false)
+    }
+
+    override fun onAuthenticationFailed() {
+        super.onAuthenticationFailed()
+        this.update("Authentication Failed", false)
+    }
+
+    override fun onAuthenticationHelp(helpMsgId: Int, helpString: CharSequence?) {
+        super.onAuthenticationHelp(helpMsgId, helpString)
+        this.update("Error$helpString", false)
+
+    }
+
+    override fun onAuthenticationSucceeded(result: FingerprintManagerCompat.AuthenticationResult?) {
+        super.onAuthenticationSucceeded(result)
+        this.update("You Can Now Access The App", false)
+    }
+
+    private fun update(string: String, boolean: Boolean) {
+        val paraLabel: TextView = (context as Activity).findViewById(R.id.prompt_message)
+        paraLabel.text = string
+        if(!boolean) {
+            paraLabel.setTextColor(ContextCompat.getColor(context, R.color.colorPrimary))
+        }
+    }
+}
